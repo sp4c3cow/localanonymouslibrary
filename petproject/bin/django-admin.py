392932ee6d99c10010cd7b474c08c45a4cd2933c
4mn3s1a <cow@co.ws>
2020-07-03 20:02:22 +0200

    final result

---
+++ petproject/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/john/DATA/DJANGO/LEARNING/Django Documentation/library/petproject/bin/python3.8
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
